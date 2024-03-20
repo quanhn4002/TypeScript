669b2d032c1d8c6ad7dfd8b81e5251eae6a17d4c
quanhn4002 <quannhph36246@fpt.edu.vn>
2024-03-20 17:54:14 +0700

    update

---
+++ React-TypeScript/src/components/banner.tsx
@@ -0,0 +1,35 @@
+import Carousel from 'react-bootstrap/Carousel'
+
+function banner() {
+  return (
+    <Carousel data-bs-theme='dark'>
+      <Carousel.Item>
+        <img
+          className='d-block w-100 h-20'
+          src='https://i.ytimg.com/vi/vMVwdSp489E/maxresdefault.jpg'
+          alt='First slide'
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='d-block w-100' src='https://picsum.photos/id/23/100/100' alt='Second slide' />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='d-block w-100' src='https://picsum.photos/id/235/100/100' alt='Third slide' />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default banner
